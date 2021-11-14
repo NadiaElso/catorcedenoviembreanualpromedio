33aec0e45f8280fd2c26232398f2381952cac1fd
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-11-14 19:08:22 -0300

    funcionaanualpromedio

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,20 @@ function promedioanual(cantidadalumnos:number) {
   
   let nota2:number[]=new Array(cantidadalumnos);
   let nota3:number[]=new Array(cantidadalumnos);
-  let promedio:number[]=new Array(cantidadalumnos);
+  let promedio:number[]=[];
 
-  promedio=0;
+
 
 
 
 
   for (let index = 0; index < cantidadalumnos; index++) {
     nombres[index]=prompt("ingrese el nombre del alumno");
-    nota1[index]=prompt("ingrese nota primer trimestre");
-    nota2[index]=prompt("ingrese nota segundo trimestre");
-    nota3[index]=prompt("ingrese nota tercer trimestre");
+    nota1[index]=Number(prompt("ingrese nota primer trimestre"));
+    nota2[index]=Number(prompt("ingrese nota segundo trimestre"));
+    nota3[index]=Number(prompt("ingrese nota tercer trimestre"));
 
-    promedio[index]=(nota1[index]+nota2[index]+nota3[index]);
+    promedio[index]=(nota1[index]+nota2[index]+nota3[index])/3;
     console.log(`El promedio del alumno ${nombres} es ${promedio}`);
 
   }
@@ -30,7 +30,7 @@ return promedioanual;
 
 }
 
-promedioanual();
+promedioanual(1);
 
 
 
